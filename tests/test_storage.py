fdae3b1b393c041e3f84ca539d2f0cf98cef98e7
Jon Mikula <jon.mikula@lacework.net>
2021-11-06 14:08:30 -0400

    removed the extra code in test_storage.py

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -14,10 +14,6 @@ def test_storing_file_locally(infile):
     """
     Writes content from one file-like to another
     """
-    #infile = tempfile.TemporaryFile('r+b')
-    #infile.write(b"Testing")
-    #infile.seek(0)
-
     outfile = tempfile.NamedTemporaryFile(delete=False)
     storage.local(infile, outfile)
     with open(outfile.name, 'rb') as f:
